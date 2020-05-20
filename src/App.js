5b81d82feca6aba396cb91105754ad19ec2ae3b3
Pawel L <virtuozoo@o2.pl>
2020-05-20 18:29:51 +0200

    refactor context

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import MapboxGLMap from "./components/MapboxGLMap/MapboxGLMap";
 import AbsoluteWrapper from "./components/AbsoluteWrapper/AbsoluteWrapper";
 import WeatherWidget from "./components/WeatherWidget/WeatherWidget";
+import MapboxGlMapContextProvider from "./context/MapboxGlMapContext";
 
 function App() {
   return (
-    <AbsoluteWrapper>
-      <MapboxGLMap />
-      <WeatherWidget />
-    </AbsoluteWrapper>
+    <MapboxGlMapContextProvider>
+      <AbsoluteWrapper>
+        <MapboxGLMap />
+        <WeatherWidget />
+      </AbsoluteWrapper>
+    </MapboxGlMapContextProvider>
   );
 }
 
